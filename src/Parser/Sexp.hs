33d7f4b30e806e5ff33103da52e00dfd1baaac01
Jimmy <jimmy123good@hotmail.com>
2021-04-27 01:45:47 -0700

    refactored

--- src/Parser/Sexp.hs
+++ src/Parser/Sexp.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+
 {-
    Pogger is a s-expression based language, transform the surface syntax to
    s-expression is the first step of the compilation.
@@ -8,15 +9,13 @@
 -}
 module Parser.Sexp where
 
-
-
-import           AST
-import           Data.Char          (digitToInt)
-import qualified Data.Map           as M
-import           Data.Maybe
-import           Numeric
-import           Text.Parsec
-import           Text.Parsec.String (Parser)
+import AST
+import Data.Char (digitToInt)
+import qualified Data.Map as M
+import Data.Maybe
+import Numeric
+import Text.Parsec
+import Text.Parsec.String (Parser)
 
 -- | Non alpha numeric characters
 poggerSymbol :: Parser Char
@@ -29,35 +28,34 @@ poggerString = do
   x <- many escapedChar
   char '"'
   return $ String x
- where
-  escapedChar =
-    escape '"'
-      <|> escape '\\'
-      <|> escape '\n'
-      <|> escape '\n'
-      <|> escape '\r'
-      <|> escape '\t'
-      <|> noneOf "\""
-  escape :: Char -> Parser Char
-  escape c = try $ do
-    char '\\'
-    char c
+  where
+    escapedChar =
+      escape '"'
+        <|> escape '\\'
+        <|> escape '\n'
+        <|> escape '\n'
+        <|> escape '\r'
+        <|> escape '\t'
+        <|> noneOf "\""
+    escape :: Char -> Parser Char
+    escape c = try $ do
+      char '\\'
+      char c
 
 -- | Atom are alphanumeric followed alphanumerics, symbols and digits.
 poggerAtom :: Parser PoggerVal
 poggerAtom = do
-  h  <- letter <|> poggerSymbol
+  h <- letter <|> poggerSymbol
   hs <- many $ letter <|> digit <|> poggerSymbol
   let atom = h : hs
   return $ case atom of
     "#t" -> Bool True
     "#f" -> Bool False
-    _    -> Atom atom
+    _ -> Atom atom
 
-{- | conform R5S5 number tower.
-Support both the integer and floating point form of
-hexdecimal (#x), binary (#d), oct (#o).
--}
+-- | conform R5S5 number tower.
+-- Support both the integer and floating point form of
+-- hexdecimal (#x), binary (#d), oct (#o).
 poggerReal :: Parser PoggerVal
 poggerReal =
   Number . Real <$> (try decFloat <|> try hexFloat <|> try octFloat <|> try binFloat)
@@ -65,20 +63,22 @@ poggerReal =
 poggerInteger :: Parser PoggerVal
 poggerInteger =
   Number . Integer
-    <$> (try decInteger <|> try hexInteger <|> try octInteger <|> try binInteger
+    <$> ( try decInteger <|> try hexInteger <|> try octInteger <|> try binInteger
         )
 
 poggerComplex :: Parser PoggerVal
-poggerComplex = sign >>= \s -> do
-  real <- (try decFloat <|> (fromIntegral <$> decInteger)) >>= \r ->
-    return $ if s == "-" then negate r else r
-  char '+'
-  img <- try decFloat <|> try (fromIntegral <$> decInteger)
-  char 'i'
-  return . Number $ Complex real img
+poggerComplex =
+  sign >>= \s -> do
+    real <-
+      (try decFloat <|> (fromIntegral <$> decInteger)) >>= \r ->
+        return $ if s == "-" then negate r else r
+    char '+'
+    img <- try decFloat <|> try (fromIntegral <$> decInteger)
+    char 'i'
+    return . Number $ Complex real img
 
 poggerRational :: Parser PoggerVal
-poggerRational =  do
+poggerRational = do
   s <- sign
   denominator <- decInteger >>= \d -> return $ if s == "-" then negate d else d
   char '/'
@@ -98,11 +98,15 @@ bin :: Parser String
 bin = many $ oneOf "01"
 
 hexInteger = prefixedFormatToInteger "#x" readHex hex
+
 octInteger = prefixedFormatToInteger "#o" readOct oct
+
 binInteger = prefixedFormatToInteger "#b" readBin bin
 
 hexFloat = prefixedFormatToFloat "#x" readHex hex
+
 octFloat = prefixedFormatToFloat "#o" readOct oct
+
 binFloat = prefixedFormatToFloat "#b" readBin bin
 
 decFloat :: Parser Double
@@ -111,36 +115,41 @@ decFloat = let num = many1 digit in read <$> sign <> num <> dot <> num
 decInteger :: Parser Integer
 decInteger = let num = many1 digit in read <$> sign <> num
 
-prefixedFormatToFloat
-  :: String -> ReadS Double -> Parser String -> Parser Double
+prefixedFormatToFloat ::
+  String -> ReadS Double -> Parser String -> Parser Double
 prefixedFormatToFloat pstr rs num = try double <|> try int
- where
-  prefix = string pstr
-  base   = case pstr of
-    "#x" -> 16
-    "#o" -> 8
-    "#b" -> 2
-  int    = prefix >> sign >>= \s -> getPart rs s <$> num
-  double = prefix >> sign >>= \s -> do
-    integral <- num
-    dot
-    fractional <- num
-    let combined = integral <> fractional
-    let d        = base ^ length fractional
-    let r        = getPart rs s combined
-    return (r / d)
+  where
+    prefix = string pstr
+    base = case pstr of
+      "#x" -> 16
+      "#o" -> 8
+      "#b" -> 2
+    int = prefix >> sign >>= \s -> getPart rs s <$> num
+    double =
+      prefix >> sign >>= \s -> do
+        integral <- num
+        dot
+        fractional <- num
+        let combined = integral <> fractional
+        let d = base ^ length fractional
+        let r = getPart rs s combined
+        return (r / d)
 
-prefixedFormatToInteger
-  :: String -> ReadS Integer -> Parser String -> Parser Integer
-prefixedFormatToInteger pstr rs num = prefix *> sign >>= \s ->
-  getPart rs s <$> num
-  where prefix = string pstr
+prefixedFormatToInteger ::
+  String -> ReadS Integer -> Parser String -> Parser Integer
+prefixedFormatToInteger pstr rs num =
+  prefix *> sign >>= \s ->
+    getPart rs s <$> num
+  where
+    prefix = string pstr
 
 -- get part of the
 getPart :: (Eq a, Num a) => ReadS a -> String -> String -> a
-getPart rs sign | sign == "-" = negate . pick
-                | otherwise   = pick
-  where pick = fst . head . rs
+getPart rs sign
+  | sign == "-" = negate . pick
+  | otherwise = pick
+  where
+    pick = fst . head . rs
 
 sign :: Parser String
 sign = (string "+" >> pure "") <|> string "-" <|> pure ""
@@ -148,11 +157,10 @@ sign = (string "+" >> pure "") <|> string "-" <|> pure ""
 dot :: Parser String
 dot = string "."
 
-{- | Parse pogger characeter
-\#<char> or \#<char name>
-Characeters in \# are self-evaluating, no need to quote them.
-A character must end with a space.
--}
+-- | Parse pogger characeter
+-- \#<char> or \#<char name>
+-- Characeters in \# are self-evaluating, no need to quote them.
+-- A character must end with a space.
 poggerChar :: Parser PoggerVal
 poggerChar = do
   char '\\' >> char '#'
@@ -161,16 +169,17 @@ poggerChar = do
     try (lkCharNames <$> choice ss) <|> anyChar
   many1 space
   return . Char $ n
- where
-  lkCharNames = fromJust . flip M.lookup charNames
-  charNames   = M.fromList
-    [ ("space"    , ' ')
-    , ("newline"  , '\n')
-    , ("tab"      , '\t')
-    , ("linefeed" , ' ')
-    , ("page"     , ' ')
-    , ("backspace", ' ')
-    ]
+  where
+    lkCharNames = fromJust . flip M.lookup charNames
+    charNames =
+      M.fromList
+        [ ("space", ' '),
+          ("newline", '\n'),
+          ("tab", '\t'),
+          ("linefeed", ' '),
+          ("page", ' '),
+          ("backspace", ' ')
+        ]
 
 -- | Parse pogger list
 poggerList :: Parser PoggerVal
@@ -180,7 +189,7 @@ poggerList = List <$> sepBy poggerExpr spaces
 poggerDottedList :: Parser PoggerVal
 poggerDottedList = do
   xs <- endBy poggerExpr spaces
-  x  <- char '.' *> spaces *> poggerExpr
+  x <- char '.' *> spaces *> poggerExpr
   return $ DottedList xs x
 
 poggerQuoted :: Parser PoggerVal
@@ -206,10 +215,11 @@ poggerNumeric =
 
 -- | Top level pogger expression parser
 poggerExpr :: Parser PoggerVal
-poggerExpr = poggerAtom <|> poggerNumeric <|> poggerQuoted <|> do
-  char '('
-  spaces
-  x <- try poggerList <|> poggerDottedList
-  spaces
-  char ')'
-  return x
+poggerExpr =
+  poggerAtom <|> poggerNumeric <|> poggerQuoted <|> do
+    char '('
+    spaces
+    x <- try poggerList <|> poggerDottedList
+    spaces
+    char ')'
+    return x
